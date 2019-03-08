b69b8affdb424874498ed9fdf1c04f7a1cc3dbcb
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-03-08 16:44:59 +0530

    1-6 Modern Setup

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',                        // '.' means the current folderwill look for dependency in which it is working
+  output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'js/bundle.js' //where to save it
+    },
+    //mode: 'development'
+    // development mode build bundle without minifying our code but production
+    devServer: {
+      contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: { //to configuer babel loader
+        rules: [
+            {
+                test: /\.js$/, //it is a regular expression, that we want to test all the js files, it will look for all the file and check weather it end with .js
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+};
