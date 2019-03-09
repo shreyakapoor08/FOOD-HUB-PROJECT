e6566c9fb56e1cc5432c39a9e385e0d3a5d37ee0
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-03-09 12:57:35 +0530

    10. making first API call

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,16 @@
-import str from './models/Search'
-//import { add as a, multiply as m, ID } from "./views/searchView";
-import * as searchView from './views/searchView';
+import axios from 'axios';
 
-console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${str}`);
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '8158269083f7617f69401d7d9f3b2c73';
+    try{
+    const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`); //axios works same as fetch but it works on all browser , it automatically return json. it is better than fetch. axios is also better in error handeling
+    const recipes = res.data.recipes;
+    console.log(recipes);
+    } catch (error){
+        alert(error);
+    }
+}
+
+getResults('pasta');
+
