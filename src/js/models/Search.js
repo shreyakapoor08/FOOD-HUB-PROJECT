459d40c032cb135eadfb5403a2dfb2290cb9b0c4
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-03-09 13:56:23 +0530

    11. building the search model

---
+++ src/js/models/Search.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = '8158269083f7617f69401d7d9f3b2c73';
+        try {
+            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`); //axios works same as fetch but it works on all browser , it automatically return json. it is better than fetch. axios is also better in error handeling
+            console.log(res);
+            this.result = res.data.recipes;
+            console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
+
+
+
