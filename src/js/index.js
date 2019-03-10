a76e97f51ccdd0c41de1272f53becb086bba5318
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-03-10 20:30:20 +0530

    14. building search view

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,40 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from "./views/base";
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+/* Global state of the app
+* - Search object
+* - Current recipe object
+* - Shopping list object
+* -Liked recipes
+
+ */
+
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = searchView.getInput(); ///To-do
+
+    if(query) {
+        // 2) New search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) render results on UI
+       searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -11,9 +11,9 @@ export default class Search {
         const key = '8158269083f7617f69401d7d9f3b2c73';
         try {
             const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`); //axios works same as fetch but it works on all browser , it automatically return json. it is better than fetch. axios is also better in error handeling
-            console.log(res);
+            //console.log(res);
             this.result = res.data.recipes;
-            console.log(this.result);
+            //console.log(this.result);
         } catch (error) {
             alert(error);
         }
