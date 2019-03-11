188fea2e8245a748e9fb559fc670d3bc482dadc2
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-03-11 22:04:30 +0530

    16.Implementing search result pagination

--- src/js/index.js
+++ src/js/index.js
@@ -30,7 +30,7 @@ const controlSearch = async () => {
 
         // 5) render results on UI
         clearLoader();
-       searchView.renderResults(state.search.result);
+        searchView.renderResults(state.search.result);
     }
 }
 
@@ -39,4 +39,12 @@ elements.searchForm.addEventListener('submit', e => {
     controlSearch();
 });
 
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if(btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10); // handy way to access the data
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
 
+    }
+});
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -8,6 +8,7 @@ export const clearInput = () => {
 
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
+    elements.searchResPages.innerHTML = '';
 };
 
 
@@ -53,6 +54,45 @@ const renderRecipe = recipe => {
             elements.searchResList.insertAdjacentHTML('beforeend',markup);
 };
 
-export const renderResults = recipes => {
-    recipes.forEach(renderRecipe);
+//type: 'prev' or 'next'
+const createButton = (page, type) => `
+        <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
+                <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+                <svg class="search__icon">
+                     <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+                </svg>
+        </button>
+        `;
+
+const renderButtons = (page, numResults, resPerPage) => {
+    const pages = Math.ceil(numResults / resPerPage);
+
+    let button;
+    if (page === 1 && pages > 1) {
+        //Only  Button to go to next page
+        button = createButton(page, 'next'); //cant use const or let here cz its scope is only inside the block
+    } else if (page < pages) {
+        // Both buttons
+        button = `
+        ${createButton(page, 'prev')}
+        ${createButton(page, 'next')}
+        `;
+    }  else if (page === pages && pages > 1) {
+        // Only button to go to prev page
+        button = createButton(page, 'prev');
+    }
+
+    elements.searchResPages.insertAdjacentHTML('afterbegin', button);
+
+}; //goal of the function is to render the buttons according to the number of the page we are on
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+    //render results of current page
+    const start = (page - 1) * resPerPage;
+    const end = page * resPerPage;
+
+    recipes.slice(start, end).forEach(renderRecipe); //slice() method returns a shallow copy of a portion of an array into  a new object selected from begin to end
+
+    //render pagination buttons
+    renderButtons(page, recipes.length, resPerPage);
 };
