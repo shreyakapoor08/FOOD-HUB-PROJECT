eb6fe2da28f1b3194f1b4ce3a7e9dabd45cc2ea3
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-03-11 23:27:09 +0530

    Building the Recipe Model - part 1

---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const key = '8158269083f7617f69401d7d9f3b2c73';
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe'
 import * as searchView from './views/searchView';
 import { elements, renderLoader, clearLoader } from "./views/base";
 
@@ -11,6 +12,10 @@ import { elements, renderLoader, clearLoader } from "./views/base";
  */
 
 const state = {};
+
+/*
+* SEARCH CONTROLLER
+ */
 
 const controlSearch = async () => {
     // 1) Get query from view
@@ -48,3 +53,10 @@ elements.searchResPages.addEventListener('click', e => {
 
     }
 });
+
+/*
+* RECIPE CONTROLLER
+ */
+const r = new  Recipe(46956);
+r.getRecipe();
+console.log(r);
