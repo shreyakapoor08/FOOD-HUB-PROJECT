5f3923c305023446401b7dcb51d48d2649d78113
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-03-13 11:55:36 +0530

    Bui;ding the recipe view 1

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -35,6 +35,7 @@ export default class Recipe {
     parseIngredients() {
         const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
         const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+        const units = [...unitsShort, 'kg', 'g']
 
         const newIngredients = this.ingredients.map(el => {
             // 1) Uniform units
@@ -48,7 +49,7 @@ export default class Recipe {
 
             // 3) Parse ingredients into count, unit and ingredient
             const arrIng = ingredient.split(' ');
-            const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2)); //includes is an array method, it returns true if the element which we pass in , is in the array
+            const unitIndex = arrIng.findIndex(el2 => units.includes(el2)); //includes is an array method, it returns true if the element which we pass in , is in the array
 
             let objIng;
             if (unitIndex > -1) {
