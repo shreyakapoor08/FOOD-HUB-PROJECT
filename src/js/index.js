c8eb895dfeec33706ba78718b06c5af4e58d4fcf
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-03-13 14:44:29 +0530

    updating recipe servings

--- src/js/index.js
+++ src/js/index.js
@@ -117,3 +117,21 @@ const controlRecipe = async () => {
 // window.addEventListener('load', controlRecipe);
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+
+//Handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+   if (e.target.matches('.btn-decrease, .btn-decrease *')) {//* means any child element of button decrease
+        //Decrease button is clicked
+       if(state.recipe.servings > 1) {
+           state.recipe.updateServings('dec');
+           recipeView.updateServingsIngredients(state.recipe);
+       }
+   } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+       //Increase button is clicked
+       state.recipe.updateServings('inc');
+       recipeView.updateServingsIngredients(state.recipe);
+
+   }
+   console.log(state.recipe);
+   });
